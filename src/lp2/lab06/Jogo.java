1369a5de5b05725e47e3c7d40bb0ca14a1d8af83
AGNALDO SOUTO XAVIER JUNIOR 115211093 <agnaldosxj@lcc2-19.lcc.ufcg.edu.br>
2016-08-11 10:28:01 -0300

    Criei a coisa

---
+++ src/lp2/lab06/Jogo.java
@@ -0,0 +1,15 @@
+package lp2.lab06;
+
+public class Jogo {
+	private String nome;
+	private double preco;
+	private int maiorScore;
+	private int qtdJogada;
+	private int zerouJogo;
+	
+	public Jogo(String nome, double preco){
+		this.nome = nome;
+		this.preco = preco;
+	}
+
+}
